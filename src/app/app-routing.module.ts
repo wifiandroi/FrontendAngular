8d6830564077459a50dc0b97eeecf8428c2b8e52
Joseph <wifiandroi@gmail.com>
2022-10-01 00:20:42 -0500

    Creando Proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './actividad/create.component';
+import { DetailsComponent } from './actividad/details.component';
+import { ListComponent } from './actividad/list.component';
+import { UpdateComponent } from './actividad/update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:ListComponent},
+  {path:'detail',component:DetailsComponent},
+  {path:'create',component:CreateComponent},
+  {path:'update',component:UpdateComponent},
+  {path:'**',redirectTo:'', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//Component
+import { ListComponent } from './actividad/list.component';
+import { DetailsComponent } from './actividad/details.component';
+import { CreateComponent } from './actividad/create.component';
+import { UpdateComponent } from './actividad/update.component';
+//External
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrService, ToastrModule } from 'ngx-toastr';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    DetailsComponent,
+    CreateComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SweetAlert2Module.forRoot(),
+    CommonModule,
+    BrowserAnimationsModule,
+     ToastrModule.forRoot(),
+     HttpClientModule,
+     FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
