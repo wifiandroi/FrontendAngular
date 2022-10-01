d4b1c411bf351eab38e6a53b543f32f2cda21ecd
Joseph <wifiandroi@gmail.com>
2022-10-01 00:46:34 -0500

    creaar menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 //Component
 import { ListComponent } from './actividad/list.component';
 import { DetailsComponent } from './actividad/details.component';
 import { CreateComponent } from './actividad/create.component';
 import { UpdateComponent } from './actividad/update.component';
+import { MenuComponent } from './menu/menu.component';
+
 //External
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrService, ToastrModule } from 'ngx-toastr';
-import { MenuComponent } from './menu/menu.component';
+
 
 
 
